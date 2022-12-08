97a3dd98654686a1e7e86a4115937f03f38edbbc
Isabella Costabile <costabileisa@gmail.com>
2022-12-08 11:58:15 -0500

    commit tree 33b42f4cff2167bd7a2529cf242dfbd4150106de

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+function superbowlWin(objArr) {
+    const foundRecord = objArr.find(obj => obj.result === "W");
+    if (!foundRecord) return;
+    return foundRecord.year;
+}
